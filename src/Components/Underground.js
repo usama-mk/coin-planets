b057d7b45fbca8bd92191007378701fe15689e7b
Usama Muhammad <usamamuhammad45678@gmail.com>
2022-02-23 07:11:52 +0500

    fixed position

--- src/Components/Underground.js
+++ src/Components/Underground.js
@@ -7,14 +7,14 @@ import Mint from './Mint'
 function Underground() {
   return (
     <div className='relative top-20'>
-        <div className="undergroundBG absolute top-0">
+        <div className="undergroundBG absolute top-40">
         <img className='' src={underGroundCont} alt="" />
 
        <div className="underContainer absolute top-0 w-full text-center">
        <div className="underTitle  text-white ">
         MINT YOUR COIN PLANT!
         </div>
-        <div className="mintWrapper w-full flex justify-center">
+        <div className="mintWrapper w-full flex justify-center mt-10">
         <Mint/>
         </div>
 
